cfea7abecc9978160eb6a51b751cb27aa7a51dbb
Aditya-Pandey-208 <pandey0208aditya@gmail.com>
2026-06-06 18:56:50 +0530

    Replace Node backend with Spring Boot backend

---
+++ prepforge-api/src/main/java/com/prepforge/prepforgeapi/PrepforgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.prepforge.prepforgeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepforgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepforgeApiApplication.class, args);
+	}
+
+}
---
+++ prepforge-api/src/test/java/com/prepforge/prepforgeapi/PrepforgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prepforge.prepforgeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepforgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
